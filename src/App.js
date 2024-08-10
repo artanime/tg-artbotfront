2dd6f0dd915b62a582172343037350c1fed076c3
artanime <artanimecustomization@gmail.com>
2024-08-10 04:24:06 -0800

    123

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-
 import './App.css';
- //const tg = window.Telegram.WebApp;
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  return (
-    <div className="App">
-v  vsdvsdvsdv
+    const {onToggleButton, tg} = useTelegram();
 
-    </div>
-  );
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
